0bedd125c06f5b1b9853cfaf60100c0a67e8284a
Rackim <110779887+HernanCortesDeMonroy@users.noreply.github.com>
2022-11-14 08:36:17 +0600

    .

---
+++ contracts/Interfaces/IERC165.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.16;
+
+interface IERC165{
+
+    supportInterface(bytes4 interfaceId) external view return(bool);
+
+}
---
+++ contracts/Interfaces/IERC721.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.16;
+
+import "./IERC165.sol";
+
+interface IERC721{
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    event Approval(address indexed owner, address indexed approved, uint256 tokenId);
+
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    function balanceOf(address owner) external view returns(unt256 balance);
+
+    function ownerOf(uint256 tokenId) external view returns(address owner);
+
+    function safeTransferFrom(
+        address from, 
+        address to, 
+        uint256 tokenId
+    ) external;
+
+    function transferFrom (
+        address from,
+        address to,
+        uint236 tokenId
+    ) external;
+
+    function approve(address to, uint256 tokenId) external;
+
+    function approveForAll(address operator, bool _approved) external;
+
+    function getApproved(uint256 tokenId) external view returns(address operator);
+
+    function isApprovedForAll(address owner, address operator) external view returns(bool);
+
+}
+
+
